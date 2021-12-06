5c05eb8ba823b1c2358b4e21b8590c11c7775a9c
gannon darcy <gkdarcy93@gmail.com>
2021-12-06 18:14:10 -0500

    added theme to main.dart, made categories screen the home page, adding fonts too

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
@@ -8,13 +10,19 @@ class CategoriesScreen extends StatelessWidget {
     // and will create as many columns as possible for those grid items to set next to each other.
     //childAspectRatio helps you set height in regard to the width (so if width is set to 200, 3/2 ratio means height will be 300)
 
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-      children: [],
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeal')),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
+      ),
     );
   }
 }
---
+++ lib/category_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+      //instead of color we use decoration for better design and allow us to use a gradient bia the box decoration class
+      //there are several types of gradients to pick, we picked linear
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [color.withOpacity(0.7), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15)),
+    );
+  }
+}
