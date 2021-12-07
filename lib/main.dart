5e05c52c9315befa79889eb033cf248d9d6a366e
gannon darcy <gkdarcy93@gmail.com>
2021-12-07 00:20:30 -0500

    added more routing for the meal details page, added a fallback route for 404 issues too, and some notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/screens/category_meals_screen.dart';
+import 'screens/category_meals_screen.dart';
+import 'screens/meal_detail_screen.dart';
 import 'screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -26,6 +27,27 @@ class MyApp extends StatelessWidget {
       routes: {
         //note that '/' is already accounted for, its for the home: CategoriesScreen() route.
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen()
+      },
+      //this takes a function that takes some route settings (like args) and then it will return a route with a builder,
+      //this is only reached when you are going to named route that is NOT registered in the routes table but uses .pushNamed
+      //I don't need it on this app (or many app) but its good to note and shows me how to access the settings of routes.
+      //mainly used for things when the app needs to dynamically generate routes that don't exist at runtime. Video 171.
+      // onGenerateRoute: (settings){
+      //   print(settings.arguments);
+      //   if (settings.name == '/meal-detail'){
+      //     return ...
+      //   } else if (settings.name == '/something-else'){
+      //     return ...
+      //   }
+      //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      // },
+
+      //takes an anon function that gets settings about the route it tried to load and which returns a route. this route is reached when fluttter
+      //failes to build a screen via all other measures (its a last resort). So the route/widget it would show would typically be a starting page or
+      // '404: we couldn't find that page, please go back or try again'
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
       },
     );
   }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/models/meal.dart';
+import '../screens/meal_detail_screen.dart';
 import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
   final int duration;
@@ -10,18 +11,56 @@ class MealItem extends StatelessWidget {
   final Affordability affordability;
 
   MealItem(
-      {@required this.title,
+      {@required this.id,
+      @required this.title,
       @required this.imageUrl,
       @required this.affordability,
       @required this.complexity,
       @required this.duration});
 
-  void selectMeal() {}
+  //using a getter to dynamically compute a variables value.
+  String get complexityText {
+    //since i'm returning in each of these cases the break statement is not actually needed. BUT if i was just doing some calculation or something for a case
+    //then I WOULD want to break otherwise its going to keep going through options and hit default too.
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Expensive';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  void selectMeal(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed(MealDetailScreen.routeName, arguments: id);
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: selectMeal,
+      onTap: () => selectMeal(context),
 
       //when you want a card to have a different shape you can do that, RoundedRectangleBorder is common.
       child: Card(
@@ -47,8 +86,66 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                //softwrap is for if the text in the card is too long it will wrap
+                //overflow set to fade so if even wrapping isn't enough, the text will be clipped in the box and fade out (shouldn't happen but this is a safety check)
+                //Positioned is a widget you can call ONLY in stacked that allows you to put a child in an absolute coordinate in the parent space. by saying how many pixels away from the bottom, right or whatever you want it to be.
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    //this container to help constrain the width of the text so the softwrap and overflow args actually work.
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
+            ),
+            //-------row of info below the image----------
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  //-------the dureaction aspect ------
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('$duration min')
+                    ],
+                  ),
+                  //--------the complexity aspect -------
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(complexityText)
+                    ],
+                  ),
+                  //--------the affordability aspect -------
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(affordabilityText)
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
